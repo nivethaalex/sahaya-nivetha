05cf8b6538a31ae3160106245b9d6f7337b11564
nivethaalex <45812711+nivethaalex@users.noreply.github.com>
2018-12-14 12:02:45 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,13 @@
+function validateform(){
+x=document.get Element By Id("Email").value
+if(x=="){
+alert("email must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("password").value
+if(x=="){
+alert("password must be filled");
+return.false
+}
+}
