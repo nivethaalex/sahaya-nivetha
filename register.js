92e668eeb2ed1aec2b38b2dc71b13a0cb42b0f73
nivethaalex <45812711+nivethaalex@users.noreply.github.com>
2018-12-14 11:58:04 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,43 @@
+function validateform(){
+x=document.get Element By Id("uses nane").value
+if(x=="){
+alert("user name must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("college name").value
+if(x=="){
+alert("college name must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("degree").value
+if(x=="){
+alert("degree must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("language know").value
+if(x=="){
+alert("language know must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("email").value
+if(x=="){
+alert("email must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("password").value
+if(x=="){
+alert("password must be filled");
+return.false
+}
+function validateform(){
+x=document.get Element By Id("repeat password").value
+if(x=="){
+alert("repeat pssword must be filled");
+return.false
+}
+}
